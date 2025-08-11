f6fa10e78f380b5bee0422a7c591bf7381f45911
ऋतिक राज गुप्ता <142139641+RITIKRAJGUPTA@users.noreply.github.com>
2025-08-11 05:38:30 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -4,10 +4,7 @@ const connectDB = async () => {
   try {
     const uri = process.env.MONGO_URI;
     if (!uri) throw new Error('MONGO_URI is not defined in .env');
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await  mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
